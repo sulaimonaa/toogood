4810bfdaa3a8acdb1081a39ad36b438a52000913
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-02-14 23:46:36 +0100

    changes in DB

--- db.js
+++ db.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2')
 
 const db = mysql.createPool({
-    connectionLimit: 10, // Allows multiple connections
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    connectionLimit: 20,
+    queueLimit: 0,
+    keepAliveInitialDelay: 10000,
+    waitForConnections: true,
+    enableKeepAlive: true,
 });
 
 
