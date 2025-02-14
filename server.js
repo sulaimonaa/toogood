649fcd92b46090388b65907da87326c21a9e57c0
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-02-14 19:45:12 +0100

    up server

--- server.js
+++ server.js
@@ -9,27 +9,24 @@ const visaRoutes = require('./routes/visaRoutes');
 const permitRoutes = require('./routes/permitRoutes');
 const insuranceRoutes = require('./routes/insuranceRoutes');
 
+
 const app = express()
 
+app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
 app.use(express.json())
 
-app.use("/uploads", express.static("uploads"))
+const port = process.env.PORT
 
+app.use("/uploads", express.static("uploads"))
 app.use('/agents', agentRoutes)
 app.use('/admin', adminRoutes)
 app.use('/visa', visaRoutes)
 app.use('/permit', permitRoutes)
 app.use('/insurance', insuranceRoutes)
 
-app.use(express.static(path.join(__dirname)));
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, "index.html"));
-});
-
-const port = process.env.PORT || 5000
-
 app.listen(port, () => {
     console.log("Server listening on port", port);
 });
+
+
