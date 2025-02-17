0d7716b573a529afc30fb1daf7a44b38a08f8a50
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-02-17 08:52:20 +0100

    update popular deals

--- src/components/Popular/PopularDeals.js
+++ src/components/Popular/PopularDeals.js
@@ -8,24 +8,24 @@ const PopularDeals = () => {
         <div className='container'>
             <h3 className='text-dark mb-3 fs-4'>Popular deals</h3>
             <div className='d-flex gap-2'>
-                <div className='col-6 pd pd-1 mb-2'>
+                <div className='col-6 pd pd-1 mb-2 pop-list'>
                     <Link to='/flight' className='bg-white opacity-75 rounded-pill p-2 text-decoration-none w-50 text-center text-dark fw-bold'>
                         Explore flights
                     </Link>
                 </div>
-                <div className='col-6 pd pd-2 mb-2'>
+                <div className='col-6 pd pd-2 mb-2 pop-list'>
                 <Link to='/hotel' className='bg-white opacity-75 rounded-pill p-2 text-decoration-none w-50 text-center text-dark fw-bold'>
                         Explore hotels
                     </Link>
                 </div>
             </div>
             <div className='d-flex gap-2'>
-                <div className='col-6 pd pd-3 mb-2'>
+                <div className='col-6 pd pd-3 mb-2 pop-list'>
                 <Link to='/e-visa' className='bg-white opacity-75 rounded-pill p-2 text-decoration-none w-50 text-center text-dark fw-bold'>
                         Get e-visa
                     </Link>
                 </div>
-                <div className='col-6 pd pd-4 mb-2'>
+                <div className='col-6 pd pd-4 mb-2 pop-list'>
                 <Link to='/e-sim' className='bg-white opacity-75 rounded-pill p-2 text-decoration-none w-50 text-center text-dark fw-bold'>
                         Get e-sims
                     </Link>
