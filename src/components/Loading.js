95bb1e9edab49c4b3c1c5627f79e9576e3a56f63
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-02-26 11:28:03 +0100

    add spinner to loader

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { SpinningCircles } from 'react-loading-icons'
 
 const Loading = ({ message }) => {
   return (
     <div className="fixed inset-0 bg-light bg-opacity-50 z-50 d-flex vw-100 vh-100 justify-content-center align-items-center" style={{ zIndex: 1000 }}>
+      <div className='d-flex gap-3 flex-column align-items-center justify-content-center'>
+      <SpinningCircles fill="#00FF00"/>
       <p>{message}</p>
+      </div>
     </div>
   );
 };
