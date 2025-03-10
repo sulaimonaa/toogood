5ee4a2f54273d6acc6fa878afe58673759b455b9
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-03-10 14:07:31 +0100

    adjust db

--- db.js
+++ db.js
@@ -5,6 +5,8 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    port: 3306,
+    connectTimeout: 20000,
     connectionLimit: 20,
     queueLimit: 0,
     keepAliveInitialDelay: 10000,
