ecd9014441d1c06520e2d8ca867eae99bdad1f79
Sulaimon Ayodeji <sulaimonaa@gmail.com>
2025-05-19 14:21:06 +0100

    add visa bookings support route

--- routes/visaRoutes.js
+++ routes/visaRoutes.js
@@ -320,6 +320,158 @@ router.post("/application", upload.fields([
     }
 });
 
+router.post("/bookings", upload.fields([
+    { name: "upload_signature", maxCount: 1 }
+]), async (req, res) => {
+    try {
+        const { 
+            first_name,
+            middle_name,
+            last_name,
+            email,
+            phone_number,
+            date_of_birth,
+            coverage_begin,
+            coverage_end,
+            destination,
+            title,
+            traveler_first_name,
+            traveler_last_name,
+            trip_type,
+            flight_details,
+            hotel_title,
+            hotel_first_name,
+            hotel_last_name,
+            visa_interview_date,
+            check_in_date,
+            check_out_date,
+            hotel_details,
+        } = req.body;
+
+        if (!first_name || !last_name || !phone_number || !email) {
+            return res.status(400).json({ message: "Missing required fields" });
+        }
+
+
+        // Store file paths
+        const upload_signature = req.files["upload_signature"] ? req.files["upload_signature"][0].filename : null;
+
+        const sql = `
+            INSERT INTO visa_bookings (
+                first_name,
+                middle_name,
+                last_name,
+                email,
+                phone_number,
+                date_of_birth,
+                coverage_begin,
+                coverage_end,
+                destination,
+                title,
+                traveler_first_name,
+                traveler_last_name,
+                trip_type,
+                flight_details,
+                hotel_title,
+                hotel_first_name,
+                hotel_last_name,
+                visa_interview_date,
+                check_in_date,
+                check_out_date,
+                hotel_details,
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+
+        const values = [
+            first_name,
+            middle_name,
+            last_name,
+            email,
+            phone_number,
+            date_of_birth,
+            coverage_begin,
+            coverage_end,
+            destination,
+            title,
+            traveler_first_name,
+            traveler_last_name,
+            trip_type,
+            flight_details,
+            hotel_title,
+            hotel_first_name,
+            hotel_last_name,
+            visa_interview_date,
+            check_in_date,
+            check_out_date,
+            hotel_details,
+            upload_signature
+        ];
+
+        db.query(sql, values, (err, result) => {
+            if (err) {
+                console.error("Database error:", err);
+                return res.status(500).json({ message: "Database error" });
+            }
+
+            // Email content
+            const mailOptions = {
+                from: '"Too Good Travels" <noreply@toogoodtravels.net>',
+                to: contact_email, 
+                cc:"toogoodtravelsnigeria@gmail.com",
+                subject: "Visa Support Booking Application Submitted Successfully",
+                html: `
+                    <div style="padding: 20px; font-family: Arial, sans-serif; background-color: #f8f8f8; border-radius: 5px;">
+                        <h2 style="color: #333;">Dear ${first_name} ${last_name},</h2>
+                        <p style="color: #555;">Thank you for submitting your visa support booking application.</p>
+
+                        <div style="background-color: #fff; padding: 15px; border-radius: 5px; border: 1px solid #ddd;">
+                            <h3 style="color: #333;">Application Details:</h3>
+                            <ul style="padding-left: 20px;">
+                                <li><strong>Full Name:</strong> ${first_name} ${last_name}</li>
+                                <li><strong>Phone Number:</strong> ${phone_number}</li>
+                                <li><strong>Email:</strong> ${email}</li>
+                                <li><strong>Destination:</strong> ${destination}</li>
+                                <li><strong>Travel Type:</strong> ${trip_type}</li>
+                                <li><strong>Travel Period:</strong> ${coverage_begin} to ${coverage_end}</li>
+                                <li><strong>Flight Details:</strong> ${flight_details}</li>
+                                <li><strong>Hotel Details:</strong> ${hotel_details}</li>
+                                <li><strong>Hotel Period:</strong> ${coverage_begin} to ${coverage_end}</li>
+                                <li><strong>Passport Data Page:</strong> <a href="https://toogood-1.onrender.com/uploads/${upload_signature}"">Download/View</a></li>
+                            </ul>
+                        </div>
+
+                        <p style="color: #555; margin-top: 20px;">We will review your application and get back to you soon.</p>
+                        <p style="color: #333; margin-bottom: 0"><strong>Best regards,</strong></p>
+                        <p style="color: #333;"><strong>Too Good Travels</strong></p>
+                    </div>
+                `,
+            };
+
+            // Send email inside the callback function
+            transporter.sendMail(mailOptions, (emailError, info) => {
+                if (emailError) {
+                    console.error("Email sending error:", emailError);
+                } else {
+                    console.log("Email sent successfully:", info.response);
+                }
+            });
+
+            res.json({ success: "Visa support booking application submitted successfully"});
+        });
+
+    } catch (error) {
+        console.error("Server error:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+});
+
+router.get('/visa-bookings', authenticateAdmin, (req, res) => {
+    const sql = "SELECT * FROM visa_bookings ORDER BY created_at DESC";
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ message: "Database error" });
+        res.json(results);
+    });
+});
+
 router.get('/pending', authenticateAdmin, (req, res) => {
     const sql = "SELECT * FROM visa_applications WHERE visa_status = 'Pending'";
     db.query(sql, (err, results) => {
